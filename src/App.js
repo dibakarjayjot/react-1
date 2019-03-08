8dd4daab3a39a54e74fa8209c4155928b723ae04
Dibakar Jayjot <dibakarjayjot@gmail.com>
2019-03-09 04:41:39 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import {DISHES } from './Shared/dishes'
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      dishes:DISHES
+    };
+
+  }
   render() {
     return (
       <div className="App">
@@ -13,7 +23,7 @@ class App extends Component {
       <NavbarBrand href="/">Dibakar Jayjot</NavbarBrand>
       </div>
       </Navbar>
-      <Menu/>
+      <Menu dishes={this.state.dishes}/>
 
         
       </div>
